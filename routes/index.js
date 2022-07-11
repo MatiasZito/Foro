31906406a67402b3e75c0ab999ee480b33d2556d
Palatta <alib.shanti@gmail.com>
2022-07-11 23:32:15 +0200

    ruta y pagina para enviar el formulario y otros cambios

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+/* GET validar sign-in. */
+router.post('/validar-signin', function(req, res, next) {
+  let name = req.body.name;
+  let surname = req.body.surname;
+  let nickname = req.body.nickname;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  console.log(email,password);
+
+  res.render('validar-signin', {name:name, surname:surname, nickname:nickname, email:email, password:password})
+});
+
 router.get('/javascript', function(req, res, next) {
   res.render('javascript');
 });
