07abbed18c0d21752921fc510301064552598485
Palatta <alib.shanti@gmail.com>
2022-07-12 14:09:40 +0200

    cambio de ruta formulario signin y solucion problema de consola

--- routes/users.js
+++ routes/users.js
@@ -15,4 +15,17 @@ router.get('/signin', function(req, res, next) {
   res.render('signin');
 });
 
+/* GET validar sign-in. */
+router.post('/validar-signin', function(req, res, next) {
+  let name = req.body.name;
+  let surname = req.body.surname;
+  let nickname = req.body.nickname;
+  let email = req.body.email;
+  let password = req.body.password;
+
+  console.log(req.body);
+
+  res.render('validar-signin', {name:name, surname:surname, nickname:nickname, email:email, password:password});
+});
+
 module.exports = router;
