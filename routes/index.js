a566e00625c03881c5351f56830df18922e7ba9a
Palatta <alib.shanti@gmail.com>
2022-07-21 15:29:39 +0200

    version con temas dinamicos

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/javascript', function(req, res, next) {
-  res.render('javascript');
+router.get('/tema/*', function(req, res, next) {
+  let path = req.path;
+  let tema = path.split('/')[2];
+  console.log(tema);
+  res.render('pagina_tema', {tema:tema});
 });
 
 module.exports = router;
