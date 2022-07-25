be41cfaeb6f49fba1f8922537928eae61928900f
MatiasZito <matiaszito@gmail.com>
2022-07-25 15:01:17 +0200

    change

---
+++ model/User.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    mail: {type:String, required: true, max:100},
+    password: {type:String, required: true, max:100},
+});
+
+
+
+module.exports = mongoose.model('User', UserSchema);
